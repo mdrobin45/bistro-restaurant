ad4e044accd38736fd2919cf58f612735be62524
Md Robin Rana <developer.smrobin45@gmail.com>
2023-11-11 06:36:59 +0600

    remove package

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import { ThemeProvider } from "@material-tailwind/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { ParallaxProvider } from "react-scroll-parallax";
 import router from "./Routes/Routes.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <React.StrictMode>
-      <ParallaxProvider>
-         <ThemeProvider>
-            <RouterProvider router={router} />
-         </ThemeProvider>
-      </ParallaxProvider>
+      <ThemeProvider>
+         <RouterProvider router={router} />
+      </ThemeProvider>
    </React.StrictMode>
 );
